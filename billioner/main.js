e4040bdb16cfba751b4622e06cdeb43991f5a289
Valerii N <nyshnui@gmail.com>
2021-12-09 14:42:58 +0200

    qwerty

---
+++ billioner/main.js
@@ -0,0 +1,24 @@
+const h1 = document.getElementById('h1');
+
+const td1 = document.getElementById('td_1');
+const td2 = document.getElementById('td_2');
+const td3 = document.getElementById('td_3');
+const td4 = document.getElementById('td_4');
+
+h1.onclick = function (){
+    console.log('click');
+}
+
+td1.onclick = function (){
+    console.log('click_td_1');
+}
+td2.onclick = function (){
+    console.log('click_td_2');
+}
+td3.onclick = function (){
+    console.log('click_td_3');
+}
+td4.onclick = function (){
+    console.log('click_td_4');
+}
+
