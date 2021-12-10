52e89c0a13ce6a94a79f54cd1e7ba66bfb4ee0f4
Valerka-php <nyshnui@gmail.com>
2021-12-10 18:06:11 +0200

    update calculator

--- billioner/main.js
+++ billioner/main.js
@@ -14,26 +14,24 @@ const questions = [
     '13. Какой предмет стал причиной смерти французского композитора Жан-Батиста Люлли?',
     '14. Что не умеют делать ящерицы гекконы? ',
 ]
-
 const asnwers = [
-    ['под ёлкой','под палкой','под скалкой','под мухой'],
-    ['кусака','закаляка','забияка','кулебяка'],
-    ['по одёжке','по сберкнижке','по прописке','по рекомендации'],
-    ['утятник','лягушатник','селёдочник','тюленник'],
-    ['починял примус','чистил обувь','варил кашу','играл в шахматы'],
-    ['11','69','77','88'],
-    ['Камп Ноу','Альберт-холл','Сан-Сиро','Энфилд'],
-    ['Операция «Арго»','Операция «Трест»','Операция «Святой Януарий»','Операция «Ы»'],
-    ['Княжна Мэри»','«Бэла»','«Княгиня Лиговская»','«Максим Максимыч»'],
-    ['дрова','свечи','спички','пледы'],
-    ['в Новгороде','в фильме «Брат 2»','в Белом Море','в соборе Парижской Богоматери'],
-    ['оконной рамы','закрытой фрамуги','дверного проёма','туалетного крючка'],
-    ['дирижёрская трость','струна рояля','гусиное перо','смычок скрипки'],
-    ['ловить рыбу','ходить по потолку','менять цвет','петь'],
+    ['под ёлкой', 'под палкой', 'под скалкой', 'под мухой'],
+    ['кусака', 'закаляка', 'забияка', 'кулебяка'],
+    ['по одёжке', 'по сберкнижке', 'по прописке', 'по рекомендации'],
+    ['утятник', 'лягушатник', 'селёдочник', 'тюленник'],
+    ['починял примус', 'чистил обувь', 'варил кашу', 'играл в шахматы'],
+    ['11', '69', '77', '88'],
+    ['Камп Ноу', 'Альберт-холл', 'Сан-Сиро', 'Энфилд'],
+    ['Операция «Арго»', 'Операция «Трест»', 'Операция «Святой Януарий»', 'Операция «Ы»'],
+    ['Княжна Мэри»', '«Бэла»', '«Княгиня Лиговская»', '«Максим Максимыч»'],
+    ['дрова', 'свечи', 'спички', 'пледы'],
+    ['в Новгороде', 'в фильме «Брат 2»', 'в Белом Море', 'в соборе Парижской Богоматери'],
+    ['оконной рамы', 'закрытой фрамуги', 'дверного проёма', 'туалетного крючка'],
+    ['дирижёрская трость', 'струна рояля', 'гусиное перо', 'смычок скрипки'],
+    ['ловить рыбу', 'ходить по потолку', 'менять цвет', 'петь'],
 ]
-
-const currentAnswer = ['под ёлкой', 'кулебяка','по одёжке','лягушатник','починял примус','77','Альберт-холл',
-    'Операция «Арго»','«Княгиня Лиговская»','спички','в Новгороде','дверного проёма','дирижёрская трость','ловить рыбу']
+const currentAnswer = ['под ёлкой', 'кулебяка', 'по одёжке', 'лягушатник', 'починял примус', '77', 'Альберт-холл',
+    'Операция «Арго»', '«Княгиня Лиговская»', 'спички', 'в Новгороде', 'дверного проёма', 'дирижёрская трость', 'ловить рыбу']
 
 let count = 0;
 let countAnswer = 0;
@@ -49,74 +47,81 @@ let countAnswer = 0;
 // question(0);
 
 
-
-
-function answer(val){
-    document.querySelector('.timer').innerHTML = 60;
+function checkAnswer(val) {
 
     let value;
 
-    if (val === 1){
-         value = document.querySelector('.answer1').innerHTML;
-    }else if (val === 2){
-         value = document.querySelector('.answer2').innerHTML;
-    }else if (val === 3){
-        value = document.querySelector('.answer3').innerHTML;
-    }else if (val === 4){
-        value = document.querySelector('.answer4').innerHTML;
+    switch (val) {
+        case 1:
+            value = document.querySelector('.answer1').innerHTML;
+            break;
+        case 2:
+            value = document.querySelector('.answer2').innerHTML;
+            break;
+        case 3:
+            value = document.querySelector('.answer3').innerHTML;
+            break;
+        case 4:
+            value = document.querySelector('.answer4').innerHTML;
+            break;
     }
 
     // console.log(typeof(value));
     // console.log(count);
 
-    if (value){
-        for (let key of currentAnswer){
-            for (i = 0; i < currentAnswer.length ; i++){
-                if (value === key){
-                    // console.log('DA')
-                    count++;
-                    document.querySelector('.question').innerHTML = questions[count];
-                    document.querySelector('.answer1').innerHTML = asnwers[count][0];
-                    document.querySelector('.answer2').innerHTML = asnwers[count][1];
-                    document.querySelector('.answer3').innerHTML = asnwers[count][2];
-                    document.querySelector('.answer4').innerHTML = asnwers[count][3];
-                    break;
-                }else {
-                    // console.log('NEA')
-                    break;
-                }
+    if (value) {
+        for (let key of currentAnswer) {
+            if (value === key) {
+                // console.log('DA')
+                count++;
+                document.querySelector('.timer').innerHTML = 15;
+                document.querySelector('.question').innerHTML = questions[count];
+
+                document.querySelector('.answer1').innerHTML = asnwers[count][0];
+                document.querySelector('.answer2').innerHTML = asnwers[count][1];
+                document.querySelector('.answer3').innerHTML = asnwers[count][2];
+                document.querySelector('.answer4').innerHTML = asnwers[count][3];
+
+                let elem = document.querySelector('.qwe')
+                console.log(elem);
+                elem.style.backgroundColor = 'green';
+                break;
+            }else {
+                document.querySelector('.start').innerHTML = 'Сыграть еще';
+                document.querySelector('.timer').innerHTML = '';
+
+                clearInterval(setTimer);
             }
         }
-    }
-    // console.log(value);
+        // console.log(value);
 
+    }
 }
 
 
 
-function start(){
+function setStart() {
+    document.querySelector('.timer').innerHTML = 15;
+    document.querySelector('.start').innerHTML = 'Start';
+
     document.querySelector('.question').innerHTML = questions[count];
     document.querySelector('.answer1').innerHTML = asnwers[count][0];
     document.querySelector('.answer2').innerHTML = asnwers[count][1];
     document.querySelector('.answer3').innerHTML = asnwers[count][2];
     document.querySelector('.answer4').innerHTML = asnwers[count][3];
 
-    // здесь я пытался циклом вывести ответы в список но они списком выпадали.
-    // Пока что только перезаписывают первый элемент списка =)
-    // for (i = 0 ; i <= 3 ; i++){
-    //    document.querySelector('.answer').innerHTML = asnwers[count][i];
-    // }
-
-    setInterval(timer,1000);
+    setInterval(setTimer, 1000);
 }
-function timer(){
+
+function setTimer() {
     let count = +document.querySelector('.timer').textContent;
-    if (count >= 1 ){
+
+    if (count >= 1) {
         count--;
         document.querySelector('.timer').innerHTML = count;
         // console.log(count)
-    }else {
-        clearInterval(timer);
+    } else {
+        clearInterval(setTimer);
     }
 }
 
